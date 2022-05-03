3982022173d2af93777ba71a8ed77087b1fe7e21
2Em4 <243042@vutbr.cz>
2022-05-03 17:13:55 +0200

    meeeh

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_nth_fibo(cislo):
+    if cislo == 0:
+        return 0
+    elif cislo == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(cislo-1)+recursive_nth_fibo(cislo-2)
 
 def main():
     pass
+    print()
 
 
 if __name__ == "__main__":
